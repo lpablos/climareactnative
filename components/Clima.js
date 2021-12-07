f3ca89b8c07522cf5d79243db2e21b7dd9223dd3
Luis Jorge <gevolutionxalapa@gmail.com>
2021-12-07 14:43:50 -0600

    Secciones del la app

---
+++ components/Clima.js
@@ -0,0 +1,12 @@
+    import React from 'react'
+    
+    const Clima = () => {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+    
+    export default Clima
+    
