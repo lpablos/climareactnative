b31fc3da3141f397b2d42f9bc5cc46c27b98c4ff
Luis Jorge <gevolutionxalapa@gmail.com>
2021-12-07 16:09:37 -0600

    Agrego date piker y el boton para hacer la peticion

--- components/Clima.js
+++ components/Clima.js
@@ -1,12 +1,16 @@
-    import React from 'react'
-    
-    const Clima = () => {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
-    
-    export default Clima
-    
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+const Clima = () => {
+  return (<Text style={styles.texto}>Hola desde el detalle del clima</Text>);
+};
+
+const styles = StyleSheet.create({
+  texto: {
+    color: '#0E0D0D',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+});
+
+export default Clima;
--- components/Formulario.js
+++ components/Formulario.js
@@ -1,11 +1,58 @@
-import React from 'react'
-
+import React,{useState} from 'react';
+import {View, Text, StyleSheet, TextInput, TouchableWithoutFeedback} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 const Formulario = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [selectedLanguage, setSelectedLanguage] = useState();
+  const [count, setCount] = useState(0);
 
-export default Formulario
+  const onPress = () => {
+    setCount(count + 1);
+  };
+  return (
+    <>
+      <View style={styles.formulario}>
+        <View>
+          <TextInput placeholder="Ciudad" />
+          <Picker>
+            <Picker.Item label="Java" value="java" />
+            <Picker.Item label="JavaScript1" value="js1" />
+            <Picker.Item label="JavaScript2" value="js2" />
+            <Picker.Item label="JavaScript3" value="js3" />
+            <Picker.Item label="JavaScript4" value="js4" />
+            <Picker.Item label="JavaScript5" value="js5" />
+          </Picker>
+        </View>
+        <TouchableWithoutFeedback onPress={onPress}>
+            <View style={styles.button}>
+                <Text>Touch Here</Text>
+            </View>
+      </TouchableWithoutFeedback>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  texto: {
+    color: '#0A0909',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  formulario:{
+    marginTop: 80,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#0A0909",
+    padding: 10
+  },
+  countContainer: {
+    alignItems: "center",
+    padding: 10
+  },
+  countText: {
+    color: "#FF00FF"
+  }
+});
+
+export default Formulario;
