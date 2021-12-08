0ea22af43b372d87e04049e8345e1f08331460ef
Luis Jorge <gevolutionxalapa@gmail.com>
2021-12-08 12:45:28 -0600

    Agrege listado del piker y animacion en el boton de buscar clima

--- App.js
+++ App.js
@@ -7,22 +7,44 @@
  */
 
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, TouchableWithoutFeedback, Keyboard} from 'react-native';
+
+
 import Formulario from './components/Formulario';
 import Clima from './components/Clima';
 
 
 const App  = () => {
+  const ocultarTeclado = () =>{
+    // ocultar tecaldo function
+    Keyboard.dismiss();
+  }
   return (
     <>
-      <View>
-        <Formulario/>
-        <Clima />
-      </View>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          ocultarTeclado();
+        }}>
+        <View style={styles.app}>
+          <View style={styles.contenido}>
+            <Formulario/>
+          </View>
+          <Clima />
+        </View> 
+      </TouchableWithoutFeedback>
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  app:{
+    flex: 1,
+    backgroundColor: 'rgb(71, 149, 212)',
+    justifyContent: 'center',
+  },
+  contenido: {
+    marginHorizontal: '2.5%',
+  }
+});
 
 export default App;
--- components/Formulario.js
+++ components/Formulario.js
@@ -1,32 +1,67 @@
-import React,{useState} from 'react';
-import {View, Text, StyleSheet, TextInput, TouchableWithoutFeedback} from 'react-native';
+import React, {useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableWithoutFeedback,
+  Animated,
+} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 const Formulario = () => {
   const [selectedLanguage, setSelectedLanguage] = useState();
   const [count, setCount] = useState(0);
+  const [animacionBtn, setAnimacionBtn] = useState(new Animated.Value(1));
 
   const onPress = () => {
     setCount(count + 1);
   };
+
+  const animacionEntrada = () =>{
+    Animated.spring(animacionBtn, {
+      toValue: .75,
+      useNativeDriver: true,
+    }).start() 
+    
+  }
+
+  const animacionSalida = () => {    
+    Animated.spring(animacionBtn, {
+      toValue: 1,
+      friction: 4,
+      useNativeDriver: true,
+      tension: 100,
+    }).start() 
+  }
+
+  const estiloAnimacion = {
+    transform: [{ scale : animacionBtn}]
+  }
   return (
     <>
       <View style={styles.formulario}>
         <View>
-          <TextInput placeholder="Ciudad" />
+          <TextInput style={styles.input} placeholder="Ciudad" />
           <Picker>
-            <Picker.Item label="Java" value="java" />
-            <Picker.Item label="JavaScript1" value="js1" />
-            <Picker.Item label="JavaScript2" value="js2" />
-            <Picker.Item label="JavaScript3" value="js3" />
-            <Picker.Item label="JavaScript4" value="js4" />
-            <Picker.Item label="JavaScript5" value="js5" />
+            <Picker.Item label="-- Seleccione un país --" value="" />
+            <Picker.Item label="Estados Unidos" value="US" />
+            <Picker.Item label="México" value="MX" />
+            <Picker.Item label="Argentina" value="AR" />
+            <Picker.Item label="Colombia" value="CO" />
+            <Picker.Item label="Costa Rica" value="CR" />
+            <Picker.Item label="España" value="ES" />
+            <Picker.Item label="Perú" value="PE" />
           </Picker>
         </View>
-        <TouchableWithoutFeedback onPress={onPress}>
-            <View style={styles.button}>
-                <Text>Touch Here</Text>
-            </View>
-      </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback
+          itemStyle={{ height: 120, backgroundColor: '#FFF'}}
+          onPressIn={ ()=> animacionEntrada() }
+          onPressOut={ () => animacionSalida() }
+          >
+          <Animated.View style={[styles.btnBuscar, estiloAnimacion]}>
+            <Text style={styles.textoBuscar}>Buscar Clima</Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
       </View>
     </>
   );
@@ -38,21 +73,37 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 30,
   },
-  formulario:{
+  formulario: {
     marginTop: 80,
   },
-  button: {
-    alignItems: "center",
-    backgroundColor: "#0A0909",
-    padding: 10
-  },
   countContainer: {
-    alignItems: "center",
-    padding: 10
+    alignItems: 'center',
+    padding: 10,
   },
   countText: {
-    color: "#FF00FF"
-  }
+    color: '#FF00FF',
+  },
+  input: {
+    padding: 10,
+    height: 50,
+    backgroundColor: '#FFF',
+    fontSize: 20,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  btnBuscar: {
+    alignItems: 'center',
+    backgroundColor: '#000',
+    padding: 10,
+    justifyContent: 'center',
+  },
+  textoBuscar: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    fontSize: 18,
+  },
 });
 
 export default Formulario;
